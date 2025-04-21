4e58cb15d9f8fc76355fcc9ba372cc70141fae0d
Elijah Abebe <ellayab804@gmail.com>
2025-04-21 05:41:18 +0000

    initial project commit

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const path = require('path')
 require('dotenv').config()
+
+const upload = multer({dest: 'uploads/'})
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+// app.use()
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file
+  if(!file) return res.status(400).json({error: "No file uploaded"})
 
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    })
+})
 
 
 const port = process.env.PORT || 3000;
